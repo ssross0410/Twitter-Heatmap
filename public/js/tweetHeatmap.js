5268b6a953a51ee31123c89c76514f6f6ec8b46f
Ross Shen <sushen410@gmail.com>
2015-10-18 10:52:46 -0400

    rename some files

--- public/js/tweetHeatmap.js
+++ public/js/tweetHeatmap.js
@@ -3,7 +3,7 @@ function initialize() {
   var NYC = new google.maps.LatLng(40.77, -73.98);
   var light_grey_style = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
  
-  var map = new google.maps.Map(document.getElementById("map_canvas"), {
+  var map = new google.maps.Map(document.getElementById("heatmap"), {
     center: NYC,
     zoom: 4,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
